b7ab4767d4aa87f77c4865409a9b4aa09f46c2d6
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-07 17:35:59 +0300

    Create database models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    post_id = models.UUIDField(default=uuid.uuid4, editable=False, )
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE )
+    body =  models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    
+    def __str__(self):
+        return f"Comment by {Comment.author} on {Comment.post}"
+    
